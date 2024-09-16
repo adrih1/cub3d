3d9e1b92afa7fd9cba96675e02d6e374f2e83ce9
Adrien Hors <ahors@made-f0Ar7s11.clusters.42paris.fr>
2024-09-16 19:01:12 +0200

    Preparing structure for texture - Going home

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: ahors <ahors@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 19:05:18 by ahors             #+#    #+#             */
-/*   Updated: 2024/09/16 18:19:42 by ahors            ###   ########.fr       */
+/*   Updated: 2024/09/16 18:59:43 by ahors            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,44 +33,47 @@
 -------------------------------------------------------------
 */
 
+// Enumération des types de tokens
+typedef enum e_texture_type
+{
+	NO,
+	SO,
+	EA,
+	WE,
+}					t_texture_type;
+
 // Structure pour la fenetre de la MLX
 typedef struct s_render_v
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
-}			t_data;
+	void			*mlx_ptr;
+	void			*win_ptr;
+}					t_data;
 
 // Structure pour stocker les infos des textures
 typedef struct s_texture
 {
-	char	*id;
-	char	*filename;
-}			t_texture;
-
-// Structure pour stocker les colors de la map
-typedef struct s_color
-{
-	char	*id;
-	char	**rgb;
-}			t_color;
+	t_texture_type	texture_id;
+	char			*filename;
+}					t_texture;
 
 // Structure pour représenter la map
 typedef struct s_map
 {
-	int		m_width;
-	int		m_height;
-	char	**dirty_grid;
-	char	**grid;
-	int		player_x;
-	int		player_y;
-	char	*north_textxure;
-	char	*east_textxure;
-	char	*south_textxure;
-	char	*west_textxure;
-	char	**f_color;
-	char	**c_color;
-	t_data	data;
-}			t_map;
+	int				m_width;
+	int				m_height;
+	char			**dirty_grid;
+	char			**grid;
+	int				player_x;
+	int				player_y;
+	char			*north_textxure;
+	char			*east_textxure;
+	char			*south_textxure;
+	char			*west_textxure;
+	t_texture		**textures;
+	char			**f_color;
+	char			**c_color;
+	t_data			data;
+}					t_map;
 
 /*
 -------------------------------------------------------------
@@ -79,42 +82,43 @@ typedef struct s_map
 */
 
 /************ INIT ************/
-int			init_mlx(t_data *data);
+int					init_mlx(t_data *data);
 
 /************ PARSING ************/
 // Debug Display - To Be Deleted
-void		ft_display_grid(t_map *map, char *grid_name);
-void		ft_display_colors(t_map *map);
-void		ft_display_map_info(t_map *map);
+void				ft_display_grid(t_map *map, char *grid_name);
+void				ft_display_colors(t_map *map);
+void				ft_display_map_info(t_map *map);
 
 // Utils
-int			ft_open_file(char *filename);
-int			ft_find_file_height(int fd);
-int			ft_map_copy_lines(int fd, t_map *map);
-char		*ft_str_trim(char *str, char c);
+int					ft_open_file(char *filename);
+int					ft_find_file_height(int fd);
+int					ft_map_copy_lines(int fd, t_map *map);
+char				*ft_str_trim(char *str, char c);
 
 // First Step - Retrieving the file lines
 // int			ft_check_trimmed_has_char(char *str); _ Pas utilise pour l'instant
 // void		ft_clean_dirty_map(t_map *map); - Pas utilise pour l'instant
-int			ft_generate_dirty_map_file(int fd, t_map *map, char *filename);
+int					ft_generate_dirty_map_file(int fd, t_map *map,
+						char *filename);
 // Second Step - Storing map info in our map structure
-char		*ft_clean_id_whitespace_color(char *str);
-int			ft_find_colors(t_map *map);
-int			ft_find_textures(t_map *map);
-int			ft_map_find_info(t_map *map);
+char				*ft_clean_id_whitespace_color(char *str);
+int					ft_find_colors(t_map *map);
+int					ft_find_textures(t_map *map);
+int					ft_map_find_info(t_map *map);
 // Third Step - Building the actual map grid
-int			ft_generate_map_file(t_map *map);
+int					ft_generate_map_file(t_map *map);
 
 // Fourth Step - Check Everything is valid
 
 // Main Function
-int			ft_parsing(t_map *map, char *filename);
+int					ft_parsing(t_map *map, char *filename);
 
 /************ EXECUTOR ************/
-int			ft_executor(t_map *map);
+int					ft_executor(t_map *map);
 
 /************ CLEANING ************/
-void		ft_clean(t_map *map);
-int			ft_message_error(char *error);
+void				ft_clean(t_map *map);
+int					ft_message_error(char *error);
 
 #endif
--- srcs/ft_parsing/ft_find_textures.c
+++ srcs/ft_parsing/ft_find_textures.c
@@ -6,27 +6,35 @@
 /*   By: ahors <ahors@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 17:10:26 by adrienhors        #+#    #+#             */
-/*   Updated: 2024/09/16 18:49:18 by ahors            ###   ########.fr       */
+/*   Updated: 2024/09/16 18:58:10 by ahors            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
 
-//Determine tetxure ID 
+//Determine tetxure ID - TODO
 
-//Get Filename of Texture 
+
+//Get Filename of Texture - TODO 
+
+
 static char	*ft_attribute_tetxure(char *str, t_map *map)
 {
 	int	i;
+	t_texture **map_textures;
 
 	i = 0;
+	map_textures = malloc(sizeof(t_texture *) * 4);
 	while (str[i])
 	{
 		// TODO
 		// Je sais pas si la il faut ajouter une protection si ft_strdup foire (en tout cas pas de leaks ni segfault)
 		if (str[i] == 'N' && str[i + 1] == 'O')
-			map->north_textxure = ft_strdup(str);
+		{
+			map_textures[i]->id = 
+			map->north_textxure = ft_strdup(str);			
+		}
 		else if (str[i] == 'S' && str[i + 1] == 'O')
 			map->south_textxure = ft_strdup(str);
 		else if (str[i] == 'E' && str[i + 1] == 'A')
